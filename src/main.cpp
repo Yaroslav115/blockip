c4f0968604361d52bfb72cbf5e418c7e83ba1479
jwretsam <jwretsam@gmail.com>
2013-10-16 17:17:36 +0300

    Structure is created.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+// blockip
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+};
